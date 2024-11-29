02d0449b9b1971f184ebf3a887b42e831a5db91b
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-11-29 21:44:38 +1300

    improve

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 def evaluate(expr, goal):
     stack = []
     for e in expr:
-        if isinstance(e, int):
+        if isinstance(e, float):
             stack.append(e)
         elif len(stack) < 1:
             return False
@@ -20,6 +20,10 @@ def evaluate(expr, goal):
             if stack[-1] < 0:
                 return False
             stack[-1] = math.sqrt(stack[-1])
+        elif e == "log":
+            if stack[-1] <= 0:
+                return False
+            stack[-1] = math.log2(stack[-1])
         elif len(stack) < 2:
             return False
         elif e == "/":
@@ -36,18 +40,27 @@ def evaluate(expr, goal):
         elif e == "-":
             stack[-2] -= stack[-1]
             del stack[-1]
+        elif e == "^":
+            if stack[-2] > 0:
+                try:
+                    stack[-2] = stack[-2] ** stack[-1]
+                except OverflowError:
+                    return False
+            del stack[-1]
     if len(stack) != 1:
         return False
     if stack[0] == 0:
         stack[0] = 0.0000000001
+    if stack[0] > 100000 or stack[0] < -100000:
+        return False
     if abs(goal / stack[0] - 1) < 0.0001:
         return stack[0]
 
 
-opts = [1, 2, 3, 4, "/", "*", "+", "-", "sin", "cos", "tan", "sqrt"]
+opts = [1.0, 2.0, 3.0, 4.0, math.pi, math.e, "/", "*", "+", "-", "^", "sin", "sqrt", "log"]
 goal = float(sys.argv[1])
 for i in range(1, 10):
     print(i)
     for e in itertools.product(opts, repeat=i):
         if ans := evaluate(e, goal):
-            print(e, ans)
+            print(f"{e} = {ans} ({ans - goal}, {ans / goal * 100}%)")
